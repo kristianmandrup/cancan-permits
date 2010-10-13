06b0427ac76b4851fc25b35c9cbc662cf973316e
yoda <yoda@tallon4.net>
2010-10-13 12:08:42 +0800

    const hack

--- lib/cancan-permits/permits/roles.rb
+++ lib/cancan-permits/permits/roles.rb
@@ -1,7 +1,8 @@
 module Permits::Roles
   def self.available
     if Module.const_defined? :User
-      User.defined_roles if User.respond_to? :defined_roles
+      [:guest, :admin]
+      #User.defined_roles if User.respond_to? :defined_roles
     else
       [:guest, :admin]
     end
